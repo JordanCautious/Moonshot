c54c392da9c1d2c08972e279fb12c8a0f436f8ea
Jordan Haynes <Jordancautious@gmail.com>
2022-04-15 15:34:53 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Jordan Haynes on 4/15/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
